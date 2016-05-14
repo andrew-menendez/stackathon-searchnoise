508d4a5dc16a5736d17b3c68ac0cb570df2e9a5a
andrewmenendez <andrew@engrade.com>
2016-05-14 14:40:18 -0400

    toggle on off

--- background.js
+++ background.js
@@ -1,61 +1,62 @@
 // React when a browser action's icon is clicked.
 
-//var red= require('./red.js');
-
 var _topic;
-
-chrome.storage.sync.get('adTopic',function(topic){
-
-  _topic=topic.adTopic;
-
-  console.log(_topic);
-})
-
-
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-console.log(_topic);
-
+var state;
 var newURL = "http://www.google.com/";
-
 var baseTab;
 
 var doSearch = function(_tab){
-    var tab=_tab;
-    console.log(baseTab.id)
+        var tab=_tab;
+        console.log(baseTab.id)
 
-    if(_tab.Id===baseTab.Id){
+        if(_tab.Id===baseTab.Id && state==="on"){
 
-      chrome.tabs.executeScript(baseTab.id,{
-        code:'var'
-      })
-
-      chrome.tabs.executeScript(baseTab.id,{file:"search.js"},function(array){
-        console.log('executed search.js');
-          setTimeout(doClick, 3000,baseTab);
-        });
-    };
-
-  };
-var doClick = function(_tab){
-  chrome.tabs.executeScript(baseTab.id,{file:"red.js"},function(array){
-
-        setTimeout(function(){
-            chrome.tabs.update(baseTab.id,{url:newURL}, function(array){
-            setTimeout(doSearch, 2000,baseTab);
+          chrome.tabs.executeScript(baseTab.id,{
+            code:'var'
           })
-        },4000);
 
-      });
+          chrome.tabs.executeScript(baseTab.id,{file:"search.js"},function(array){
+            console.log('executed search.js');
+              setTimeout(doClick, 3000,baseTab);
+            });
+        };
 };
 
-  chrome.tabs.create({ url: newURL }, function(_tab){
-    console.log(_tab);
-    console.log(_tab.id);
-    baseTab=_tab;
-    console.log(baseTab);
-    doSearch(_tab);
-  });
+var doClick = function(_tab){
+
+    if(state==="on") {
+      chrome.tabs.executeScript(baseTab.id,{file:"red.js"},function(array){
+
+            setTimeout(function(){
+                chrome.tabs.update(baseTab.id,{url:newURL}, function(array){
+                setTimeout(doSearch, 2000,baseTab);
+              })
+            },4000);
+
+          });
+    };
+};
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+  console.log(_topic);
+
+  if(state!="on"){
+      chrome.tabs.create({ url: newURL }, function(_tab){
+        state="on";
+        console.log(_tab.id);
+        baseTab=_tab;
+        console.log(baseTab);
+        doSearch(_tab);
+        chrome.browserAction.setBadgeText({text:"on"})
+      });
+
+  }// end state if !on;
+  else if (state==="on"){
+    chrome.tabs.remove(baseTab.id, function() {
+        state="off";
+        chrome.browserAction.setBadgeText({text:""})
+     });
+  };
 
 });
